68d8d7fda08b769423b8423f9447375060bb659f
lukatsotskolauri <lukawowkolauri@gmail.com>
2023-01-09 12:33:59 +0400

    fixing portfolio links

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,26 +9,32 @@ const Portfolio = () => {
     {
       id: 1,
       src: arryDestruct,
+      link: ""
     },
     {
       id: 2,
       src: instalNode,
+      link: ""
     },
     {
       id: 3,
       src: navbar,
+      link: ""
     },
     {
       id: 4,
       src: Portfolioo,
+      link: ""
     },
     {
       id: 5,
       src: reactWeather,
+      link: ""
     },
     {
       id: 6,
       src: Portfolioo,
+      demo: ""
     },
   ];
 
@@ -46,20 +52,17 @@ const Portfolio = () => {
         </div>
 
         <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolio.map(({ id, src }) => (
+          {portfolio.map(({ id, src, demo }) => (
             <div key={id} className=" shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
-                className=" rounded-md duration-200 hover:scale-105"
+                className=" rounded-md duration-200 hover:scale-105 h-4/6 w-full"
               />
               <div className=" flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
-                </button>
+                  <a href={demo} target="_blank" rel="noopener noreferrer " className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-center" >Demo</a> 
+                
+                  <a href={demo} target="_blank" rel="noopener noreferrer " className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 text-center" >Code</a> 
               </div>
             </div>
           ))}
